6d818017e1d15f87e97fc27f1253834b0ad392ba
Dan Sela <20563290+dan505512@users.noreply.github.com>
2022-12-29 10:46:55 +0200

    Update sandboxPendo.js

--- src/static/js/sandboxPendo.js
+++ src/static/js/sandboxPendo.js
@@ -9,7 +9,7 @@
 
 $(document).ready(() => {
   // Initialize only in Sandbox
-  if (document.location.hostname.endsWith('sandbox.nodejs.demo.rookout.com')) {
+  if (document.location.hostname.endsWith('sandbox-nodejs.rookout-demo.com/')) {
     pendo.initialize();
   }
 });

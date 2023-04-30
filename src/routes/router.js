e58dd4df52e1a9ba37d6da086d4531fe6037015e
roman-rookout <127094865+roman-rookout@users.noreply.github.com>
2023-04-30 18:11:48 +0300

    w

--- src/routes/router.js
+++ src/routes/router.js
@@ -18,8 +18,10 @@ router.delete('/todos/:id', ActionsHandlers.deleteTodo);
 router.post('/todos/:id/duplicate', ActionsHandlers.duplicateTodo);
 router.delete('/todos', ActionsHandlers.clearCompletedTodos);
 router.post('/todos/toggleall', ActionsHandlers.toggleAllTodos);
+const globalvar = 'globalvar';
 router.get('/test', (req, res) => {
-    console.log('test will work')
+    const localvar = 'localvar'
+    console.log(`test will work and here is global ${globalvar}, or local ${localvar}`)
 })
 
 
